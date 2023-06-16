6c0b548290f63a7ad177759ba43291f50faeb950
211003 <122693062+211003@users.noreply.github.com>
2023-06-16 06:54:03 +0530

    Add files via upload

---
+++ first.java
@@ -0,0 +1,37 @@
+package assignments.first;
+
+import java.util.Scanner;
+
+public class first {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        int a = sc.nextInt();
+       if (a==1) {
+        System.out.println("jan");
+       } else if (a==2){
+        System.out.println("feb");
+       } else if (a==3){
+        System.out.println("march4");
+       } else if (a==4){
+        System.out.println("aprail");
+       } else if (a==5){
+        System.out.println("mey");
+       } else if (a==6){
+        System.out.println("jun");
+       } else if (a==7){
+        System.out.println("julai");
+       } else if (a==8){
+        System.out.println("agast");
+       } else if (a==9){
+        System.out.println("september");
+       } else if (a==10){
+        System.out.println("october");
+       } else if (a==11){
+        System.out.println("november");
+       } else if (a==12){
+        System.out.println("desember");
+       }
+    }
+   
+    
+}
